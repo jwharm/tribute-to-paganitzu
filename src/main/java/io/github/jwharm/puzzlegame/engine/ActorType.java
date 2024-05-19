e65822c90a38d2dc61f61a55f8a4c5ed6136014d
Jan-Willem Harmannij <jwharmannij@gmail.com>
2024-05-19 21:43:15 +0200

    Update ActorTypes enum

--- src/main/java/io/github/jwharm/puzzlegame/engine/ActorType.java
+++ src/main/java/io/github/jwharm/puzzlegame/engine/ActorType.java
@@ -1,38 +1,22 @@
 package io.github.jwharm.puzzlegame.engine;
 
-import java.util.NoSuchElementException;
-
 public enum ActorType {
-    OUT_OF_BOUNDS(0),
-    BOULDER(1),
-    EMPTY(2),
-    DOOR_LOCKED(3),
-    DOOR_UNLOCKED(4),
-    GEM(5),
-    HIDDEN_PASSAGE(6),
-    KEY(7),
-    MUD(8),
-    PIPE(9),
-    PLAYER(10),
-    SNAKE(11),
-    SPIDER(12),
-    SPIKES(13),
-    WALL(14),
-    WARP(15),
-    WATER(16);
-
-    private final int id;
-
-    ActorType(int id) {
-        this.id = id;
-    }
-
-    public int id() {
-        return id;
-    }
-
-    public static ActorType of(int id) {
-        for (ActorType a : values()) if (a.id == id) return a;
-        throw new NoSuchElementException();
-    }
+    OUT_OF_BOUNDS,
+    BOULDER,
+    EMPTY,
+    DOOR_LOCKED,
+    DOOR_UNLOCKED,
+    GEM,
+    HAT,
+    HIDDEN_PASSAGE,
+    KEY,
+    MUD,
+    PIPE,
+    PLAYER,
+    SNAKE,
+    SPIDER,
+    SPIKES,
+    WALL,
+    WARP,
+    WATER;
 }
